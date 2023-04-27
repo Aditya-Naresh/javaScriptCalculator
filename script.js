99fde3295534936dda0776eae0e3f1de91cb4e7c
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-27 22:41:32 +0530

    Addition and subtraction is working multiplication and division isn't. Have to checkout and re-do the logic

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
+let num1;
+let operator = ""
+let num2;
+let result;
 function btnClick(data) {
     let display = document.getElementById('displayLabel');
-    display.value += data;
+    let value = display.value.trim();
+    if (data === "+" || data === "-" || data === "/" || data === "*") {
+        num1 = parseFloat(value);
+        operator = data;
+        display.value = ""
+
+    } else if (data === "=") {
+        num2 = parseFloat(value);
+        result = calculateResult()
+        display.value = result;
+        num1 = result;
+        num2 = 0;
+        operator = ""
+    } else {
+        value += data;
+        display.value = value
+    }
 }
 
 function clr() {
     let display = document.getElementById('displayLabel');
     display.value = ''
+    num1 = 0;
+    num2 = 0;
+    operator = ""
+}
+
+function calculateResult() {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "/":
+            return num1 / num2;
+        case "*":
+            return num1 * num2;
+        default:
+            return 0;
+    }
 }
