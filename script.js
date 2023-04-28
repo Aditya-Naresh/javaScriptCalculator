632a5072d3d542ec4c6685b8885bc4d23d955361
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-28 10:46:56 +0530

    There is some logical error in adding multiple buttons have to check it out later

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
 let num1;
-let operator = ""
+let operator = "";
 let num2;
 let result;
+let firstNumberEntered = false;
+let decimalEntered = false; // added variable to track if decimal has been entered
+
 function btnClick(data) {
     let display = document.getElementById('displayLabel');
     let value = display.value.trim();
     if (data === "+" || data === "-" || data === "/" || data === "*") {
-        num1 = parseFloat(value);
-        operator = data;
-        display.value = ""
-
+        if (!firstNumberEntered) {
+            num1 = parseFloat(value);
+            operator = data;
+            display.value = "";
+            firstNumberEntered = true;
+            decimalEntered = false; // reset decimalEntered when new number is entered
+        } else {
+            num2 = parseFloat(value);
+            result = calculateResult();
+            display.value = result;
+            num1 = result;
+            num2 = 0;
+            operator = data;
+            decimalEntered = false; // reset decimalEntered when new number is entered
+        }
     } else if (data === "=") {
         num2 = parseFloat(value);
-        result = calculateResult()
+        result = calculateResult();
         display.value = result;
         num1 = result;
         num2 = 0;
-        operator = ""
+        operator = "";
+        firstNumberEntered = false;
+        decimalEntered = false; // reset decimalEntered when calculation is performed
+    } else if (data === ".") {
+        if (!decimalEntered) { // only add decimal if it hasn't been added before
+            value += data;
+            display.value = value;
+            decimalEntered = true;
+        }
     } else {
         value += data;
-        display.value = value
+        display.value = value;
     }
 }
 
 function clr() {
     let display = document.getElementById('displayLabel');
-    display.value = ''
+    display.value = '';
     num1 = 0;
     num2 = 0;
-    operator = ""
+    operator = "";
+    firstNumberEntered = false;
+    decimalEntered = false; // reset decimalEntered when calculator is cleared
 }
 
 function calculateResult() {
@@ -41,7 +65,5 @@ function calculateResult() {
             return num1 / num2;
         case "*":
             return num1 * num2;
-        default:
-            return 0;
     }
 }
